1c4392ee5b53e5f24d3f3590757151ad68c63465
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-03-15 23:38:03 +0900

    コメントやテーブルの追加

--- migration/migration.sql
+++ migration/migration.sql
@@ -1,14 +1,37 @@
+-- pragma encoding = "UTF-8;
+-- pragma foreign_keys = ON;
+
+-- ファイル情報
 create table files (
     id integer primary key autoincrement not null unique,
     sha2 blob not null unique,
+    file_size integer not null,
+    file_offset integer not null,
     createdat datetime not null
 );
 
+-- タグ
 create table tags (
     id integer primary key autoincrement not null unique,
     name text not null unique
 );
 
+-- 言語
+create table nations (
+    id integer primary key autoincrement not null unique,
+    name text not null unique
+);
+
+-- 国際化されたタグ
+create table localized_tags (
+    tagid integer primary key autoincrement not null unique,
+    nationid integer not null,
+    name text not null unique,
+    foreign key (tagid) references tags(id),
+    foreign key (nationid) references nations(id)
+);
+
+-- タグとファイルの中間テーブル
 create table tags2file (
     fileid integer not null,
     tagid integer not null,
@@ -16,17 +39,23 @@ create table tags2file (
     foreign key (tagid) references tags(id)
 );
 
+-- 優先度
 create table permutations (
     fileid integer not null unique,
     ranking integer not null default 0
 );
 
+-- 優先度をインクリメント
 update permutations set rank = rank + 1
 from permutation inner join files on files.id = ?;
 
+-- 優先度をデクリメント
+update permutations set rank = rank - 1
+from permutation inner join files on files.id = ?;
 
+-- データの挿入
 insert all
-    insert into files (sha2, createdat) values (?, ?)
+    insert into files (sha2, file_size, file_offset, createdat) values (?, ?, ?, ?)
     insert into permutations (fileid)
         select files.id from files.sha2 = ?,
     
@@ -40,3 +69,10 @@ insert all
         where tags.name = ? and files.sha2 = ?;
     -- 繰り返しここまで
 select * from DUAL;
+
+-- タグ名からファイルを検索する
+select files.file_size, files.file_offset from files, tags2file as t2f
+where t2f.fileid = files.id and t2f.tagid = 
+    (select tags.name from tags where tags.name in (?, ?, ..., ?))
+offset ? limit ? order by files.id ?;
+
